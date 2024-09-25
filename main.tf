0180ca851f843f7fd4c49ca8163fa4776066d4fd
Sacha Wharton <32266273+sachajw@users.noreply.github.com>
2024-09-25 11:59:44 +0200

    🛠 NEW: k8s 1.18

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ provider "kind" {
 
 resource "kind_cluster" "ortelius" {
   name            = var.kind_cluster_name
-  node_image      = "kindest/node:v1.25.3"
+  #node_image      = "kindest/node:v1.25.3"
+  node_image      = "kindest/node:v1.18.0"
   kubeconfig_path = pathexpand(var.kind_cluster_config_path)
   wait_for_ready  = true
 
